fd7bb5430983fdb71066d9aac0ba7306c58d9941
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-04-28 23:54:14 +0700

    added api edit room

--- application/controllers/api/Admin.php
+++ application/controllers/api/Admin.php
@@ -112,6 +112,114 @@ class Admin extends CI_Controller
 			echo json_encode($respoonse);
 		}
 	}
+
+	public function updateRoom()
+	{
+
+		$pathImage = $this->input->post('path_image');
+		$roomId = $this->input->post('room_id');
+
+		$config['upload_path'] = './assets/upload/';
+		$config['allowed_types'] = 'jpg|jpeg|png';
+		$config['max_size'] = '2080';
+
+		$this->load->library('upload', $config);
+		$data2 = array();
+		$this->upload->initialize($config);
+
+		$pathImage = $this->input->post('path_image');
+		$roomId = $this->input->post('room_id');
+
+		if ($pathImage == '') {
+
+			$data = [
+				'kode_tipe' => $this->input->post('kode_tipe'),
+				'merek' => $this->input->post('room_name'),
+				'no_plat' => $this->input->post('kapasitas'),
+				'warna' => $this->input->post('dekorasi'),
+				'tahun' => $this->input->post('tahun'),
+				'status' => $this->input->post('status'),
+				'harga' => $this->input->post('harga'),
+				'denda' => $this->input->post('denda'),
+				'ac' => $this->input->post('ac'),
+				'sopir' => $this->input->post('sopir'),
+				'mp3_player' =>  $this->input->post('mp3_player'),
+				'central_lock' => $this->input->post('central_lock')
+			];
+			$insert = $this->ruangan_model->updateRuangan($roomId, $data);
+			if ($insert == true) {
+				$response = [
+					'status' => true,
+					'code' => 200
+				];
+				echo json_encode($response);
+			} else {
+				$response = [
+					'status' => false,
+					'code' => 400,
+					'message' => 'Gagal mengubah data'
+				];
+				echo json_encode($response);
+			}
+		} else {
+
+			if ($this->upload->do_upload('gambar')) {
+				$data2['gambar'] = $this->upload->data('file_name');
+				$gambar = true;
+			} else {
+				$response = [
+					'status' => false,
+					'code' => 400,
+					'message' => 'Format file tidak sesuai'
+
+				];
+				echo json_encode($response);
+				return;
+			}
+
+			// Cek apakah kedua upload berhasil
+			if ($gambar == true) {
+
+				$data = [
+					'kode_tipe' => $this->input->post('kode_tipe'),
+					'merek' => $this->input->post('room_name'),
+					'no_plat' => $this->input->post('kapasitas'),
+					'warna' => $this->input->post('dekorasi'),
+					'tahun' => $this->input->post('tahun'),
+					'status' => $this->input->post('status'),
+					'harga' => $this->input->post('harga'),
+					'denda' => $this->input->post('denda'),
+					'ac' => $this->input->post('ac'),
+					'sopir' => $this->input->post('sopir'),
+					'mp3_player' =>  $this->input->post('mp3_player'),
+					'central_lock' => $this->input->post('central_lock'),
+					'gambar' => $data2['gambar']
+				];
+				$insert = $this->ruangan_model->updateRuangan($roomId, $data);
+				if ($insert == true) {
+					$response = [
+						'status' => true,
+						'code' => 200
+					];
+					echo json_encode($response);
+				} else {
+					$response = [
+						'status' => false,
+						'code' => 400,
+						'message' => 'Gagal mengubah data'
+					];
+					echo json_encode($response);
+				}
+			} else {
+				$response = [
+					'status' => false,
+					'code' => 400,
+					'message' => 'Gagal mengubah data'
+				];
+				echo json_encode($response);
+			}
+		}
+	}
 }
 
 
